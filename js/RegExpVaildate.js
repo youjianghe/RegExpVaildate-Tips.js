66056229070f188bb30ec54087e86bd57aca3a9f
youjianghe <youjianghe@foxmail.com>
2017-08-02 10:49:00 +0800

    更新： 1、优化身份证验证函数 2、增加提示框手动关闭 3、其他代码优化

--- js/RegExpVaildate.js
+++ js/RegExpVaildate.js
@@ -3,14 +3,12 @@
 @boolean woff true||false,设定是否去掉首尾空格
  */
 var RegExpVaildate = {
-
 	rule:{
 		"mobile":/^13[0-9]{9}$|14[0-9]{9}$|15[0-9]{9}$|17[0-9]{9}$|18[0-9]{9}$/,	//检查手机号
 		"email":/^[\w\+\-]+(\.[\w\+\-]+)*@[a-z\d\-]+(\.[a-z\d\-]+)*\.([a-z]{2,4})$/i,	//检查email
 		"qq":/^[1-9][0-9]{4,10}$/,	//检查QQ号
 		"hans":/^[\u4e00-\u9fa5]{1,}$/,	//检查是否为汉字
-		"number":/^[0-9]*$/,
-		"bankcard":/^(998801|998802|622525|622526|435744|435745|483536|528020|526855|622156|622155|356869|531659|622157|627066|627067|627068|627069)\d{10}$/,//银行卡校验
+		"number":/^[0-9]*$/
 	},
 	mobile:function(str,woff){
 		return RegExpVaildate.validate(str,'mobile',woff);
@@ -27,9 +25,6 @@ var RegExpVaildate = {
 	number:function(str,woff){
 		return RegExpValidate.validate(str,'number',woff);
 	},
-	bankcard:function(str,woff){
-		return RegExpValidate.validate(str,'bankcard',woff);
-	},
 	trim:function(str){
 		return str ? str.replace(/^\s+|\s+$/g,"") : str;
 	},
@@ -40,69 +35,42 @@ var RegExpVaildate = {
 		woff ? str = RegExpVaildate.trim(str,woff) : "";
 		return RegExpVaildate['rule'][regkey].test(str);
 	},
-	idcard:function(idcard) { //身份证号码校验
-	    // 1 "验证通过!", 0 //身份证号码校验错误
-	    var area = { 11: "北京", 12: "天津", 13: "河北", 14: "山西", 15: "内蒙古", 21: "辽宁", 22: "吉林", 23: "黑龙江", 31: "上海", 32: "江苏", 33: "浙江", 34: "安徽", 35: "福建", 36: "江西", 37: "山东", 41: "河南", 42: "湖北", 43: "湖南", 44: "广东", 45: "广西", 46: "海南", 50: "重庆", 51: "四川", 52: "贵州", 53: "云南", 54: "西藏", 61: "陕西", 62: "甘肃", 63: "青海", 64: "宁夏", 65: "新疆", 71: "台湾", 81: "香港", 82: "澳门", 91: "国外" };
-	    var idcard, Y, JYM;
-	    var S, M;
-	    var idcard_array = [];
-    	idcard_array = idcard.split('');
-	    //地区检验
-	    if (area[parseInt(idcard.substr(0, 2))] == null) {
-	        return false;
-	    }
-	    //身份号码位数及格式检验
-	    switch (idcard.length) {
-	        case 15:
-	            if ((parseInt(idcard.substr(6, 2)) + 1900) % 4 == 0 || ((parseInt(idcard.substr(6, 2)) + 1900) % 100 == 0 && (parseInt(idcard.substr(6, 2)) + 1900) % 4 == 0)) {
-	                ereg = /^[1-9][0-9]{5}[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|[1-2][0-9]))[0-9]{3}$/;//测试出生日期的合法性
-	            } else {
-	                ereg = /^[1-9][0-9]{5}[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|1[0-9]|2[0-8]))[0-9]{3}$/;//测试出生日期的合法性
-	            }
-	            if (ereg.test(idcard)) {
-	                return true;
-	            }
-	            else {
-	                return false;
-	            }
-	        case 18:
-	            //18位身份号码检测
-	            //出生日期的合法性检查
-	            //闰年月日:((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|[1-2][0-9]))
-	            //平年月日:((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|1[0-9]|2[0-8]))
-	            if (parseInt(idcard.substr(6, 4)) % 4 == 0 || (parseInt(idcard.substr(6, 4)) % 100 == 0 && parseInt(idcard.substr(6, 4)) % 4 == 0)) {
-	                ereg = /^[1-9][0-9]{5}19[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|[1-2][0-9]))[0-9]{3}[0-9Xx]$/;//闰年出生日期的合法性正则表达式
-	            } else {
-	                ereg = /^[1-9][0-9]{5}19[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|1[0-9]|2[0-8]))[0-9]{3}[0-9Xx]$/;//平年出生日期的合法性正则表达式
-	            }
-	            if (ereg.test(idcard)) {//测试出生日期的合法性
-	                //计算校验位
-	                S = (parseInt(idcard_array[0]) + parseInt(idcard_array[10])) * 7
-	                + (parseInt(idcard_array[1]) + parseInt(idcard_array[11])) * 9
-	                + (parseInt(idcard_array[2]) + parseInt(idcard_array[12])) * 10
-	                + (parseInt(idcard_array[3]) + parseInt(idcard_array[13])) * 5
-	                + (parseInt(idcard_array[4]) + parseInt(idcard_array[14])) * 8
-	                + (parseInt(idcard_array[5]) + parseInt(idcard_array[15])) * 4
-	                + (parseInt(idcard_array[6]) + parseInt(idcard_array[16])) * 2
-	                + parseInt(idcard_array[7]) * 1
-	                + parseInt(idcard_array[8]) * 6
-	                + parseInt(idcard_array[9]) * 3;
-	                Y = S % 11;
-	                M = "F";
-	                JYM = "10X98765432";
-	                M = JYM.substr(Y, 1);//判断校验位
-	                if (M == idcard_array[17]) {
-	                    return true; //检测ID的校验位
-	                } else {
-	                    return false;
-	                }
-	            } else {
-	                return false;
-	            }
-
-	        default:
-	            return false;
-	    }
+	idcard:function(id) { //身份证号码校验
+	   // 1 "验证通过!", 0 //校验不通过
+		var rule ={
+			'format':/^[1-9]\d{16}[0-9X]$/,
+			'birthday':/^[1-2][0-9]{3}(([0][1-9])|([1][0-2]))(([0][1-9])|([1-2][0-9])|([3][0-1]))$/  //没有判断大小月的情况
+		}
+		//号码规则校验
+		if(!rule.format.test(id)){
+			return {'status':0,'msg':'格式错误:18位,X大写,不得有空格'};
+		}
+		//省份校验
+		var province_code = { 11: "北京", 12: "天津", 13: "河北", 14: "山西", 15: "内蒙古", 21: "辽宁", 22: "吉林", 23: "黑龙江", 31: "上海", 32: "江苏", 33: "浙江", 34: "安徽", 35: "福建", 36: "江西", 37: "山东", 41: "河南", 42: "湖北", 43: "湖南", 44: "广东", 45: "广西", 46: "海南", 50: "重庆", 51: "四川", 52: "贵州", 53: "云南", 54: "西藏", 61: "陕西", 62: "甘肃", 63: "青海", 64: "宁夏", 65: "新疆", 71: "台湾", 81: "香港", 82: "澳门", 91: "国外" };
+		if(!province_code[parseInt(id.substr(0,2))]){
+			return {'status':0,'msg':'省份代码错误'}
+		}
+		//区位码校验
+		
+		//出生年月日校验
+		var now_year = parseInt((new Date()).getFullYear());
+		var year = parseInt(id.substr(6,4));
+		var birthday = id.substr(6,8);
+		if(!rule.birthday.test(birthday)||year<1800||year>now_year){
+			return {'status':0,'msg':'出生日期填写错误'}
+		}
+		//校验码判断
+		var c = new Array(7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2);   //系数
+    	var b = new Array(1,0,'X',9,8,7,6,5,4,3,2);  //校验码对照表
+		var id_array = id.split("");
+		var sum = 0;
+		for(var k=0;k<17;k++){
+			sum+=parseInt(id_array[k])*parseInt(c[k]);
+		}
+		if(id_array[17] != b[sum%11]){
+			return {'status':0,'msg':'身份证校验错误'}
+		}
+		return {'status':1,'msg':'校验通过'}
 	}
 }
 
--- js/simpleTips.js
+++ js/simpleTips.js
@@ -6,9 +6,10 @@ var simpleTips = {
 	 */
 	tips : function(msg,time){
 		var simpleTips = document.createElement('div'),body = document.body;
-		simpleTips.setAttribute('style','position:fixed;z-index:10000;top: 30%;width: 160px;left: 50%;margin-left: -60px;text-align: center; background-color: #484848;border-radius:4px;color: #ffffff;font-size: 18px;line-height:22px; word-break: break-all;font-family: "Microsoft Yahei",Arial, Verdana, Helvetica, sans-serif;');
+		simpleTips.setAttribute("id","simpleTips");
+		simpleTips.setAttribute('style','position:fixed;z-index:10000;top: 30%;width: 260px;left: 50%;margin-left: -60px;text-align: center; background-color: #484848;border-radius:4px;color: #ffffff;font-size: 18px;line-height:22px; word-break: break-all;font-family: "Microsoft Yahei",Arial, Verdana, Helvetica, sans-serif;');
 		simpleTips.innerHTML = '<p style="margin: 10px;">'+msg+'</p>';
-		if (time == ''||time == undefined) {time = 3000;}
+		if (!/^[1-9][0-9]*$/.test(time)) {time = 3000;}
 		body.appendChild(simpleTips);
 		setTimeout(function(){
 			body.removeChild(simpleTips);
@@ -16,16 +17,17 @@ var simpleTips = {
 	},
 	/*
 	@msg[string],提示信息，必须
-	@title[string],提示标题，非必须，默认为信息
-	@fun1[object],确定按钮函数
-	@fun2[object],取消按钮函数
-	 *//
+	@title[string],提示标题，非必须，默认为'信息'
+	@fun1[object],确定按钮函数,可以为空,没有返回值或为false则自动关闭,返回值为true则需手动关闭;
+	@fun2[object],取消按钮函数,其余同上
+	 */
 	alert : function(msg,title,fun1,fun2){
 		var simpleAlert = document.createElement('div'),
 			body = document.body,
 			cover = document.createElement('div'),
 		    title = title||"信息";
-		simpleAlert.setAttribute('style','position: fixed;z-index: 10000;top: 30%;width: 260px;left: 50%;margin-left: -130px;height: auto;background-color: #FFFFFF;');
+		    simpleAlert.setAttribute("id","simpleTipsAlert");
+		simpleAlert.setAttribute('style','position: fixed;z-index: 10000;top: 30%;width: 300px;left: 50%;margin-left: -130px;height: auto;background-color: #FFFFFF;');
 		simpleAlert.innerHTML  = 
 			'<div style="padding: 0 80px 0 20px;height:42px;line-height:42px;color: #333333;font-size: 14px; border-bottom:1px solid #EEE;background-color:#F8F8F8;text-align: left;">'+title+'</div>'+
 			'<div style="padding: 20px;font-size: 16px;">'+msg+'</div>'+
@@ -34,21 +36,35 @@ var simpleTips = {
 			'<button type="button" id="rh_cancel_button" style="background-color: #2e8ded;border: none;padding: 0;height: 28px;padding: 0 15px;margin: 0 6px;font-size: 14px;color: #ffffff;font-weight: 700;border-radius: 2px;cursor:pointer;">取消</button>'+
 			'</div>';
 		cover.setAttribute('style','position: fixed;z-index: 9990;top: 0;left: 0;right: 0;bottom: 0; background-color: rgba(0,0,0,.3);');
+		cover.setAttribute("id","simpleTipsMask");
 		body.appendChild(simpleAlert);
 		body.appendChild(cover);
 		document.getElementById('rh_confirm_button').onclick = function(){
-			if(typeof fun1 === 'function'){
-				fun1();
-			}
-			body.removeChild(cover);
-			body.removeChild(simpleAlert);
+			simpleAlertClose(fun1)
 		}
 		document.getElementById('rh_cancel_button').onclick = function(){
-			if(typeof fun2 === 'function'){
-				fun2();
-			}
-			body.removeChild(cover);
-			body.removeChild(simpleAlert);
+			simpleAlertClose(fun2)
 		}
+		function simpleAlertClose(fun){
+			if(typeof fun !== 'function'||!fun()){
+				simpleTips.closeAll();
+			}
+		}
+	},
+	/*
+	@关闭所有弹窗，包括tips;
+	*/
+	closeAll:function(){
+		var a = document.getElementById('simpleTipsAlert'),
+			s = document.getElementById('simpleTipsMask'),
+			t = document.getElementById('simpleTips'),
+			b = document.body;
+			if(a&&s){
+				b.removeChild(a);
+				b.removeChild(s);
+			}
+			if(t){
+				b.removeChild(t);
+			}
 	}
 }
